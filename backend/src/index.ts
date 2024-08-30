aaf486ffef8bf3a198692efa569bba16314a6800
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-30 13:05:02 +0500

    feat: Added Packages

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import authroute from "./routes/auth.route";
-import messageroute from "./routes/message.route";
-import dotenv from "dotenv";
-const app = express();
+import express from 'express'
+import authroute from './routes/auth.route'
+import messageroute from './routes/message.route'
+import dotenv from 'dotenv'
+const app = express()
 dotenv.config()
-const port = 3000;
+const port = 3000
 
-app.use("/api/auth", authroute);
-app.use("/api/messages", messageroute);
+app.use('/api/auth', authroute)
+app.use('/api/messages', messageroute)
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
