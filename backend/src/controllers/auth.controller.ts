4ca02a2fe6c2f1f62a73bef5cfba89fb2c25f2de
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-08-30 16:01:56 +0500

    feat: Added Signup Route

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,4 +1,52 @@
-import { Request, Response } from "express";
-export const signup = async (req: Request, res: Response) => {};
-export const login = async (req: Request, res: Response) => {};
-export const logout = async (req: Request, res: Response) => {};
+import { Request, Response } from 'express'
+import prisma from '../../db/prisma'
+import bycrptjs from 'bcryptjs'
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { userName, fullName, password, confirmPassword, gender } = req.body
+    if (!userName || !fullName || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: 'Please Fill All the Fields ' })
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: 'Passwords Do Not Match' })
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        userName,
+      },
+    })
+    if (user) {
+      return res.status(400).json({ error: 'User Already Exists' })
+    }
+
+    const salt = await bycrptjs.genSalt(10)
+    const hashedPassword = await bycrptjs.hash(password, salt)
+    // Api i used https://avatar.iran.liara.run/public/boy?username
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`
+
+    const newUser = await prisma.user.create({
+      data: {
+        fullName,
+        userName,
+        gender,
+        password: hashedPassword,
+        profilePic: gender === 'male' ? boyProfilePic : girlProfilePic,
+      },
+    })
+    if (newUser) {
+      return res.status(200).json({
+        id: newUser.id,
+        fullName: newUser.fullName,
+        userName: newUser.userName,
+        gender: newUser.gender,
+        profilePic: newUser.profilePic,
+      })
+    }
+  } catch (error: any) {
+    console.log('Error in signup Controller', error.message)
+    return res.status(500).json({ error: 'Error in Creating User' })
+  }
+}
+export const login = async (req: Request, res: Response) => {}
+export const logout = async (req: Request, res: Response) => {}
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 const app = express()
 dotenv.config()
 const port = 3000
+app.use(express.json())
 
 app.use('/api/auth', authroute)
 app.use('/api/messages', messageroute)
