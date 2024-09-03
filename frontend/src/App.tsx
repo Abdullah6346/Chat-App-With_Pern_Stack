3ba7a9de5ef8a7a75ab2fdfe107c7af5ced5503d
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-09-03 10:59:07 +0500

    feat: Added Ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-
-
+import { Route, Routes } from 'react-router-dom'
 function App() {
-
   return (
-    <>
-   
-    </>
+    <main className="container-main p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />}>
+          {' '}
+        </Route>
+        <Route path="/signup" element={<Signup />}>
+          {' '}
+        </Route>
+        <Route path="/login" element={<Login />}>
+          {' '}
+        </Route>
+      </Routes>
+    </main>
   )
 }
 
