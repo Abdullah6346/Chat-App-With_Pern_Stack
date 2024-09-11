b2178586f3e1b37f9f41c8817c1e7c2a22a9eaaa
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-09-11 14:33:51 +0500

    feat(updated login): updated login

--- frontend/src/hooks/useLogin.tsx
+++ frontend/src/hooks/useLogin.tsx
@@ -1,30 +1,33 @@
-import { useState } from 'react'
-import { useAuthContext } from '../context/AuthContext'
-import toast from 'react-hot-toast'
+import { useState } from "react";
+import { useAuthContext } from "../context/AuthContext";
+import toast from "react-hot-toast";
 
 const useLogin = () => {
-  const [loading, setLoading] = useState(false)
-  const { setAuthUser } = useAuthContext()
+  const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
   const login = async (userName: string, password: string) => {
     try {
-      setLoading(true)
-      const res = await fetch('/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ userName, password }),
-      })
-      const data = await res.json()
-      if (!res.ok) throw new Error(data.error)
-      setAuthUser(data)
+      setLoading(true);
+      const res = await fetch(
+        "https://chat-app-withpernstack-production.up.railway.app//api/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userName, password }),
+        }
+      );
+      const data = await res.json();
+      if (!res.ok) throw new Error(data.error);
+      setAuthUser(data);
     } catch (error: any) {
-      toast.error(error.message)
+      toast.error(error.message);
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
-  return { loading, login }
-}
+  };
+  return { loading, login };
+};
 
-export default useLogin
+export default useLogin;
