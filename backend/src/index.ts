0e8b08a4df933628a7cdb9748279aed6fb28482a
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-09-11 17:00:40 +0500

    feat: added cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,15 @@ import dotenv from 'dotenv'
 import authroute from './routes/auth.route'
 import cookieParser from 'cookie-parser'
 import { app, server } from './socket/socket'
+import cors from 'cors'
+
+const corsOptions = {
+  origin: 'https://chat-app-with-pern-stack-frontend.vercel.app', // Frontend domain
+  credentials: true, // Allow cookies
+}
+
+app.use(cors(corsOptions))
+
 dotenv.config()
 const port = process.env.PORT || 3000
 app.use(express.json())
