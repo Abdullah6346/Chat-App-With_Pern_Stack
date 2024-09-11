6fa39c62158ad2d84b75e3ea030f95e67057f449
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-09-11 17:45:10 +0500

    feat: added protected route

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
 import { login, logout, signup, getMe } from '../controllers/auth.controller'
-// import protectRoute from '../middlewares/protect.Route'
+import protectRoute from '../middlewares/protect.Route'
 const router = Router()
 // api/auth/login
-router.get('/me', getMe)
+router.get('/me', protectRoute, getMe)
 router.post('/signup', signup)
 router.post('/login', login)
 router.post('/logout', logout)
