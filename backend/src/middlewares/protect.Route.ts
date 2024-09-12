ed8f851824e733c41cd28500fa5036ed9de42ad4
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-09-12 12:55:21 +0500

    feat: added protected route

--- backend/src/middlewares/protect.Route.ts
+++ backend/src/middlewares/protect.Route.ts
@@ -1,53 +1,60 @@
 import jwt, { JwtPayload } from 'jsonwebtoken'
 import { Request, Response, NextFunction } from 'express'
 import prisma from '../../db/prisma'
-interface decodedToken extends JwtPayload {
+
+interface DecodedToken extends JwtPayload {
   userId: string
 }
+
 declare global {
   namespace Express {
     export interface Request {
-      user: {
+      user?: {
         id: string
+        fullName: string
+        profilePic: string
+        userName: string
       }
     }
   }
 }
+
 const protectRoute = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
   try {
-    const token = req.cookies.jwt
+    const token = req.cookies.jwt || req.headers['authorization']?.split(' ')[1]
+
     if (!token) {
       return res
         .status(401)
-        .json({ error: 'Unauthorized - Token Not Provided ' })
+        .json({ error: 'Unauthorized - Token Not Provided' })
     }
-    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as decodedToken
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as DecodedToken
 
     if (!decoded || !decoded.userId) {
       return res.status(401).json({ error: 'Unauthorized - Invalid Token' })
     }
 
     const user = await prisma.user.findUnique({
-      where: {
-        id: decoded.userId,
-      },
+      where: { id: decoded.userId },
       select: { id: true, fullName: true, profilePic: true, userName: true },
     })
 
     if (!user) {
-      res.status(401).json({ error: 'User Not Found ' })
+      return res.status(401).json({ error: 'User Not Found' })
     }
 
-    user ? (req.user = user) : null
+    req.user = user
 
     next()
   } catch (error: any) {
-    console.log('Error in Protected Route', error.message)
+    console.error('Error in Protected Route:', error.message)
     res.status(500).json({ error: 'Internal Server Error' })
   }
 }
+
 export default protectRoute
