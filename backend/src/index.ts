948f103d02691098694fa9a0a803f45218c727f4
Muhammad Abdullah <139040726+Abdullah6346@users.noreply.github.com.>
2024-09-12 10:47:28 +0500

    feat: removed cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,19 +4,19 @@ import dotenv from 'dotenv'
 import authroute from './routes/auth.route'
 import cookieParser from 'cookie-parser'
 import { app, server } from './socket/socket'
-import cors from 'cors'
+// import cors from 'cors'
 
 dotenv.config()
 const port = process.env.PORT || 3000
 
-const corsOptions = {
-  origin: 'https://chat-app-with-pern-stack-frontend.vercel.app', 
-  credentials: true,
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'], 
-}
+// const corsOptions = {
+//   origin: 'https://chat-app-with-pern-stack-frontend.vercel.app', 
+//   credentials: true,
+//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'], 
+// }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(cookieParser())
